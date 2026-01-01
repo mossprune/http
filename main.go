3afaedc24d0ff8afc038683f1a0459eec7323b34
moss <250817288+mossprune@users.noreply.github.com>
2026-01-02 01:49:08 +0530

    refactor to use goroutine

--- main.go
+++ main.go
@@ -8,36 +8,52 @@ import (
 	"strings"
 )
 
-func main() {
-	file, err := os.Open("text.txt")
-	if err != nil {
-		log.Fatal("Unable to open file")
-		return
-	}
-	defer file.Close()
+func getLinesChannel(f io.ReadCloser) <-chan string {
+
+	lineChan := make(chan string, 1)
 
 	buf := make([]byte, 8)
 	currentLine := ""
 
-	for {
-		n, err := file.Read(buf)
-		if err != nil {
-			if err == io.EOF {
-				break
+	go func() {
+		defer f.Close()
+		defer close(lineChan)
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatal()
 			}
-			log.Fatal()
+
+			chunks := string(buf[:n])
+			parts := strings.Split(chunks, "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				lineChan <- (string(currentLine + parts[i]))
+				currentLine = ""
+			}
+			currentLine += parts[len(parts)-1]
 		}
 
-		chunks := string(buf[:n])
-		parts := strings.Split(chunks, "\n")
-
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s\n", currentLine+parts[i])
-			currentLine = ""
+		if currentLine != "" {
+			lineChan <- (string(currentLine))
 		}
-		currentLine += parts[len(parts)-1]
+	}()
+
+	return lineChan
+}
+
+func main() {
+	file, err := os.Open("text.txt")
+	if err != nil {
+		log.Fatal("Unable to open file")
 	}
-	if currentLine != "" {
-		fmt.Printf("read: %s\n", currentLine)
+
+	lineChan := getLinesChannel(file)
+
+	for val := range lineChan {
+		fmt.Printf("read: %s\n", val)
 	}
 }
